1dfbaf8c6bf8eddd33a78a915fea6fda028fbcf6
Pedro <pedrooareas06@gmail.com>
2026-02-26 03:39:50 -0300

    Pequenos ajustes feito

--- website/src/components/productsTS/products.ts
+++ website/src/components/productsTS/products.ts
@@ -1,11 +1,11 @@
-import product1 from "@/assets/product-1.jpg";
-import product2 from "@/assets/product-2.jpg";
-import product3 from "@/assets/product-3.jpg";
-import product4 from "@/assets/product-4.jpg";
-import product5 from "@/assets/product-5.jpg";
-import product6 from "@/assets/product-6.jpg";
-import product7 from "@/assets/product-7.jpg";
-import product8 from "@/assets/product-8.jpg";
+import product1 from "../../assets/product-1.jpg";
+import product2 from "../../assets/product-2.jpg";
+import product3 from "../../assets/product-3.jpg";
+import product4 from "../../assets/product-4.jpg";
+import product5 from "../../assets/product-5.jpg";
+import product6 from "../../assets/product-6.jpg";
+import product7 from "../../assets/product-7.jpg";
+import product8 from "../../assets/product-8.jpg";
 
 export interface Product {
   id: string;
